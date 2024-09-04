9e69d7c18c2344ad676fa00369612ba90e52f357
Olajide <clickcom007@yahoo.com>
2024-09-04 12:42:28 +0100

    navbar update

--- next-app/src/app/components/Navbar.jsx
+++ next-app/src/app/components/Navbar.jsx
@@ -1,16 +1,42 @@
-import React from 'react';
+"use client"
+import React, {useState} from 'react';
 import Link from 'next/link';
+import NavLink from "./NavLink";
+import { title } from 'process';
+import path from 'path';
+import { link } from 'fs';
+
+const navLinks = [
+    {
+        title: "About",
+        path: "#about",
+    },
+    {
+        title: "Projects",
+        path: "#projects",
+    },
+    {
+        title: "Contact",
+        path: "#contact",
+    }
+
+]
 
 const Navbar = () => {
   return (
-    <nav>
-        <div className='flex flex-wrap items-center justify-between mx-auto p-8'>
-            <Link href={"/"} className='text-5xl text-white font-semibold'>
+    <nav className='fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90'>
+        <div className='flex flex-wrap items-center justify-between mx-auto px-4'>
+            <Link href={"/"} className='text-2xl md:text-5xl text-white font-semibold'>
                 LOGO
             </Link>
-            <div className='menu'>
-                <ul>
-                    <li><Link href={"about"}>About</Link></li>
+            <div className='menu block md:w-auto' id='navbar'>
+                <ul className='flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0'>
+                  {navLinks.map((link, index) => (
+                    <li key={index}>
+                      <NavLink href={link.path} title={link.title}/>
+                    </li>
+                     ))
+                   }   
                 </ul>
             </div>
         </div>
